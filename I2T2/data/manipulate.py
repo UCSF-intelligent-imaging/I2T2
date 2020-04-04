0527bd7b291462df2089eea834a8b44a131746c1
Pablo Damasceno <pablo.damasceno@ucsf.edu>
2020-04-04 11:03:20 -0700

    docstring trials

--- I2T2/data/manipulate.py
+++ I2T2/data/manipulate.py
@@ -15,11 +15,11 @@ def dcm2array(path_to_dicom_dir=None, sort_by_slice_location=True):
     Transform DICOM data into numpy array.
 
     Attributes:
-        - path_to_dicom_dir (str): Path to folder containing all dicom files for one patient
-        - sort_by_slice_location (bool): Whether to return array ordered by slice location
+            path_to_dicom_dir (str): Path to folder containing all dicom files for one patient
+            sort_by_slice_location (bool): Whether to return array ordered by slice location
 
     Returns:
-        - pixel_array (arr): Array of pixel data
+            pixel_array (arr): Array of pixel data
     """
     try:
         import gdcm
